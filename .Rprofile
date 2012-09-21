9c1a68f0abe35b78f509af445c9e607946fbda9a
Edwin de Jonge <edwindjonge@gmail.com>
2012-09-21 11:05:54 +0200

    Added package information

---
+++ .Rprofile
@@ -0,0 +1,8 @@
+.First <- function(){
+  # this code runs when the RStudio project is loaded. 
+  # It makes all using devtools very easy.
+  if (require(devtools)){
+    pkg <- as.package(".")
+    cat("Use 'load_all()' to load ',pkg$package,')
+  }
+}
--- R/pkg.R
+++ R/pkg.R
@@ -1,8 +1,9 @@
 #' abc for R
 #' 
-#' @docType package
 #' This is a package description file. It really helps your users
 #' if you add a introduction to your package
 #' @section Introduction : 
 #' This is an introduction to abc
-NULL
+#' @name AbcForR
+#' @docType package
+{}
